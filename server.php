b238a6a0b7cd3361fc6aac3458fe32cd9e14360f
Hill1942 <ykdacd123@gmail.com>
2017-04-05 13:29:10 +0800

    init

--- server.php
+++ server.php
@@ -1,56 +1,17 @@
 <?php
-/**
- * Created by kaidi
- * Date: 17-3-28
- */
 
-class Server
-{
-    private $serv;
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-    public function __construct() {
-        $this->serv = new swoole_http_server("127.0.0.1", 9501);
-        $this->serv->set(array(
-            'worker_num' => 2,
-            'daemonize' => false,
-        ));
-
-        /*$this->serv->on('Start', array($this, 'onStart'));
-        $this->serv->on('Connect', array($this, 'onConnect'));
-        $this->serv->on('Receive', array($this, 'onReceive'));
-        $this->serv->on('Close', array($this, 'onClose'));*/
-
-        $this->serv->on('request', function ($request, $response) {
-            $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
-        });
-
-        $this->serv->start();
-    }
-
-   /* public function onStart(swoole_server $serv ) {
-        echo "Start\n";
-    }
-
-    public function onConnect(swoole_server $serv, $fd, $from_id ) {
-        $serv->send($fd, "Hello {$fd}!" );
-        //$serv->send()
-    }
-
-    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
-        echo "Get Message From Client {$fd}:{$data}\n";
-        $serv->send($fd, $data);
-        //$serv->send
-    }
-
-    public function onClose(swoole_server $serv, $fd, $from_id ) {
-        echo "Client {$fd} close connection\n";
-    }*/
-
-    public function onWorkerStart(swoole_server $serv , $worker_id) {
-        require_once "EntryCGI.php";
-
-        (new EntryCGI($this->serv))->run();
-    }
+if ($uri !== '/' && file_exists(__DIR__ . '/web' . $uri)) {
+    return false;
 }
-// 启动服务器
-$server = new Server();
+
+require __DIR__ . '/vendor/autoload.php';
+
+$server = new blink\server\CgiServer([
+    'bootstrap' => __DIR__ . '/src/bootstrap.php'
+]);
+
+$server->run();
