1b7c1a7b901aa4f71d9190bc32f95b4916b9f67b
Hill1942 <ykdacd123@gmail.com>
2017-04-13 20:16:38 +0800

    test insert

--- src/http/controllers/ApiController.php
+++ src/http/controllers/ApiController.php
@@ -10,26 +10,21 @@ class ApiController extends Object
 {
     public function upload(Request $request)
     {
-        $type = $request->params->get('type'); // 获取 Query 参数 type
-        $params = $request->params->all(); // 获取所有 Query 参数
-
-        $name = $request->body->get('name'); // 获取 Request Body 的 name 参数
-        $body = $request->body->all(); // 获取整个 Request Body
+        $longtitude = $request->params->get('longitude');
+        $latitude = $request->params->get('latitude');
 
 
         $db = Database::get();
 
-        echo "a";
-
         $db->insert("tbTest", array(
-            "sOpenId" => "wihdgadglag"
+            "sOpenId" => md5("user" . time()),
+            "sLongtitude" => $longtitude,
+            "sLatitude" => $latitude
         ));
 
-        /*return [
-            "type" => $type,
-            "params" => $params,
-            "name" => $name,
-            "body" => $body
-        ];*/
+        return [
+            "code" => 0,
+            "msg" => "ok"
+        ];
     }
 }
