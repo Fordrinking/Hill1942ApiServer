dfbdf38223d59170b8622084e96751a4ecc8639a
Hill1942 <ykdacd123@gmail.com>
2017-05-07 20:05:24 +0800

    insert on duplicate

--- src/http/controllers/ApiController.php
+++ src/http/controllers/ApiController.php
@@ -5,23 +5,29 @@ namespace app\http\controllers;
 use blink\core\Object;
 use blink\http\Request;
 use app\dao\Database;
+use \PDO;
 
 class ApiController extends Object
 {
     public function upload(Request $request)
     {
-        $openid = $request->params->get('openid');
-        $longtitude = $request->params->get('longitude');
-        $latitude = $request->params->get('latitude');
+        $openid    = $request->params->get('openid');
+        $longitude = $request->params->get('longitude');
+        $latitude  = $request->params->get('latitude');
 
         if ($openid && strlen($openid) == 32) {
             $db = Database::get();
 
-            $db->insert("tbTest", array(
-                "sOpenId" => $openid,
-                "sLongtitude" => $longtitude,
-                "sLatitude" => $latitude
-            ));
+            $stmt = $db->prepare('INSERT INTO tbTest(sOpenId, sLongitude, sLatitude) ' .
+                'VALUES(:openid, :longitude, :latitude)' .
+                'ON DUPLICATE KEY UPDATE sLongitude=:lnt, sLatitude=:lat');
+
+            $stmt->bindParam(':openid',     $openid,     PDO::PARAM_STR);
+            $stmt->bindParam(':longitude',  $longitude, PDO::PARAM_STR);
+            $stmt->bindParam(':latitude',   $latitude,  PDO::PARAM_STR);
+            $stmt->bindParam(':lnt',        $longitude, PDO::PARAM_STR);
+            $stmt->bindParam(':lat',        $latitude,  PDO::PARAM_STR);
+            $stmt->execute();
 
             return [
                 "code" => 0,
